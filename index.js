92175e8cb7cb1124b728a615984a82ff8276664d
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-06-07 22:54:21 +0300

    array-find-method lab finish

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W");
+
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  const record = [
+    { year: 2014, result: "L" },
+    { year: 2015, result: "W" },
+    { year: 2016, result: "L" }
+  ];
+  
+  console.log(superbowlWin(record));
